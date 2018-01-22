d36641abd7f73b9295461f1906fba75fb50ae4c3
IdanShasha <idanshasha7@gmail.com>
2018-01-22 14:31:50 +0200

    new v

--- bundle.js
+++ bundle.js
@@ -67,7 +67,7 @@
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-(function webpackMissingModule() { throw new Error("Cannot find module \"./src/index.js\""); }());
+(function webpackMissingModule() { throw new Error("Cannot find module \"./crypto/src/index.js\""); }());
 (function webpackMissingModule() { throw new Error("Cannot find module \"./index.js\""); }());
 (function webpackMissingModule() { throw new Error("Cannot find module \"/Users/michalkohav/Downloads/cryp/bundle.js\""); }());
 
