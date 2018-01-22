a9045ce95e3c0eb3fe902f175401bf0606ebc7f0
IdanShasha <idanshasha7@gmail.com>
2018-01-22 20:53:00 +0200

    asd

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry:  './src/index.js',
     output: {
-        path:  __dirname,
+        path:  __dirname+'/crypto',
         filename: 'bundle.js',
         publicPath: '/'
     },
